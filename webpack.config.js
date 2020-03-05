b129884f632df34272f7456631b95d3cdce09135
bart <bart.delrue@digipolis.gent>
2020-03-05 16:53:07 +0100

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: __dirname + '/src/index.js',
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'modal.js',
+    library: 'Modal',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: {
+          presets: [['@babel/preset-env', {modules: 'commonjs'}]],
+          plugins: ['add-module-exports']
+        }
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin()]
+  }
+};
