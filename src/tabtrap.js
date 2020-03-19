4ec5d4b3d25a898c71c80abf63fb0c2ff0842a99
bart <bart.delrue@digipolis.gent>
2020-03-19 14:49:48 +0100

    Updated tabtrap.js to exclude hidden elements from the focusables list.

--- src/tabtrap.js
+++ src/tabtrap.js
@@ -23,14 +23,24 @@ function TabTrap (container) {
       'button:not([disabled]), ' +
       '[tabindex="0"]'
     );
-    return Array.prototype.slice.call(focusables);
+    return Array.prototype.slice.call(focusables).filter(isVisible);
   }
+
+  /**
+   * Checks if a DOM element is visible.
+   * Hidden elements cannot receive focus.
+   *
+   * @param elem
+   * @return {boolean}
+   */
+  function isVisible(elem) {return elem.offsetWidth > 0 || elem.offsetHeight > 0 || elem.getClientRects().length > 0; }
 
   this.setFocusables = function () {
     focusables = getFocusables(container);
   };
 
   this.next = function (e) {
+    this.setFocusables();
     let active = document.activeElement;
     if (active && active === focusables[focusables.length - 1]) {
       focusables[0].focus();
@@ -39,6 +49,7 @@ function TabTrap (container) {
   };
 
   this.back = function (e) {
+    this.setFocusables();
     let active = document.activeElement;
     if (active && active === focusables[0]) {
       focusables[focusables.length - 1].focus();
@@ -47,10 +58,12 @@ function TabTrap (container) {
   };
 
   this.home = function () {
+    this.setFocusables();
     focusables[0].focus();
   };
 
   this.end = function () {
+    this.setFocusables();
     focusables[focusables.length - 1].focus();
   };
 
